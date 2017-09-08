6458045a2b2a0b8f549172d758bd4698cf5f9abf
Péter Hauszknecht <peter.hauszknecht@guidesmiths.com>
2017-09-08 22:10:37 +0200

    wip

---
+++ components/Head.js
@@ -0,0 +1,7 @@
+import Head from 'next/head';
+export default () => (
+  <Head>
+    <title>corp-check</title>
+    <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.2.12/semantic.min.css" />
+  </Head>
+);
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,19 @@
 import React from 'react';
+import Head from '../components/Head';
+import { Button } from 'semantic-ui-react'
+import { Input } from 'semantic-ui-react'
 
 export default class extends React.PureComponent {
   render() {
-    return <div>Hello</div>;
+    return (
+      <div>
+        <Head />
+        <div className="ui buttons">
+          <Button size='huge' className="ui button">NPM package</Button>
+          <Button size='huge' className="ui button">Your package.json</Button>
+        </div>
+        <Input size='massive' action='Search' placeholder='Search...' />
+      </div>
+    );
   }
 }
