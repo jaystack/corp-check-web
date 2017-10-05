10786335fa83b9a98498792a040a2190fe977aa2
Péter Hauszknecht <peter.hauszknecht@guidesmiths.com>
2017-10-05 17:42:40 +0200

    resolve https://github.com/jaystack/corp-check-web/issues/2

--- components/Tree.js
+++ components/Tree.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Popup, Label } from 'semantic-ui-react';
+import { Popup, Label, Icon } from 'semantic-ui-react';
 import getColorByScore from '../utils/getColorByScore';
 import getPercentage from '../utils/getPercentage';
 
@@ -28,27 +28,48 @@ const getPopupContent = evaluations =>
   );
 
 export default class Tree extends React.PureComponent {
+  state = {
+    isOpen: (this.props.depth || 0) < 1
+  };
+
+  handleOpenClick = () => {
+    this.setState({ isOpen: !this.state.isOpen });
+  };
+
   renderNode() {
-    const { nodeName, nodeVersion, nodeScore } = this.props.node;
+    const { isOpen } = this.state;
+    const { node: { nodeName, nodeVersion, nodeScore, dependencies } } = this.props;
     return (
       <div className="node" style={{ backgroundColor: getColorByScore(nodeScore) }}>
         <span className="name">
           {nodeName}@{nodeVersion}
         </span>
         <span className="score">{getPercentage(nodeScore)}%</span>
+        <span className="icon">
+          {dependencies.length > 0 && (
+            <Icon
+              name={isOpen ? 'chevron circle right' : 'chevron circle left'}
+              size="large"
+              onClick={this.handleOpenClick}
+            />
+          )}
+        </span>
       </div>
     );
   }
 
   render() {
-    const { dependencies, evaluations } = this.props.node;
+    const { depth = 0, node: { dependencies, evaluations } } = this.props;
+    const { isOpen } = this.state;
     const popupContent = getPopupContent(evaluations);
     return (
       <div className="tree">
         {popupContent.length > 0 ? <Popup trigger={this.renderNode()}>{popupContent}</Popup> : this.renderNode()}
-        <div className="dependencies">
-          {dependencies.map(dependency => <Tree key={dependency.nodeName} node={dependency} />)}
-        </div>
+        {isOpen && (
+          <div className="dependencies">
+            {dependencies.map(dependency => <Tree key={dependency.nodeName} node={dependency} depth={depth + 1} />)}
+          </div>
+        )}
       </div>
     );
   }
