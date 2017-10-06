d140e8a9cba6f964ff41bdabab00b88df0453dd1
Péter Hauszknecht <peter.hauszknecht@guidesmiths.com>
2017-10-06 17:14:40 +0200

    Show warnings button

--- components/Summary.js
+++ components/Summary.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Container, Grid, Statistic, Item, Message } from 'semantic-ui-react';
+import { Container, Statistic, Message, Button } from 'semantic-ui-react';
 import getSummary from '../utils/getSummary';
 
 const getIconByType = type => {
@@ -19,26 +19,45 @@ export default class extends React.PureComponent {
     rootEvaluation: PropTypes.any.isRequired
   };
 
+  state = {
+    showWarnings: false
+  };
+
+  handleShowWarningsClick = () => {
+    this.setState({ showWarnings: true });
+  };
+
+  renderMessage = ({ type, evaluation, message, path }, index) => {
+    return (
+      <Message
+        key={index}
+        size="tiny"
+        negative={type === 'ERROR'}
+        warning={type === 'WARNING'}
+        icon={getIconByType(type)}
+        header={message}
+        content={path.join(' > ')}
+      />
+    );
+  };
+
   render() {
     const { rootEvaluation } = this.props;
     const { items, errorCount, warningCount } = getSummary(rootEvaluation);
+    const errors = items.filter(({ type }) => type === 'ERROR');
+    const warnings = items.filter(({ type }) => type === 'WARNING');
+    const showWarnings = errors.length <= 10 || this.state.showWarnings;
     return (
       <Container>
         <Statistic.Group>
           <Statistic color="red" value={errorCount} label="Errors" />
           <Statistic color="orange" value={warningCount} label="Warnings" />
         </Statistic.Group>
-        {items.map(({ type, evaluation, message, path }, index) => (
-          <Message
-            key={index}
-            size="tiny"
-            negative={type === 'ERROR'}
-            warning={type === 'WARNING'}
-            icon={getIconByType(type)}
-            header={message}
-            content={path.join(' > ')}
-          />
-        ))}
+        {errors.map(this.renderMessage)}
+        {showWarnings
+          ? warnings.map(this.renderMessage)
+          : warnings.length > 0 &&
+              <Button color="orange" content="Show warnings" onClick={this.handleShowWarningsClick} />}
       </Container>
     );
   }
