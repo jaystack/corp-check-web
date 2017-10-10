81cffa4d354d291b69f6984bdf6c069000e2a31c
Péter Hauszknecht <peter.hauszknecht@guidesmiths.com>
2017-10-10 11:19:08 +0200

    update showWarnings behaviour

--- components/Summary.js
+++ components/Summary.js
@@ -43,10 +43,10 @@ export default class extends React.PureComponent {
 
   render() {
     const { rootEvaluation } = this.props;
+    const { showWarnings } = this.state;
     const { items, errorCount, warningCount } = getSummary(rootEvaluation);
     const errors = items.filter(({ type }) => type === 'ERROR');
     const warnings = items.filter(({ type }) => type === 'WARNING');
-    const showWarnings = errors.length <= 10 || this.state.showWarnings;
     return (
       <Container>
         <div>
@@ -61,10 +61,13 @@ export default class extends React.PureComponent {
           </Statistic.Group>
         </div>
         {errors.map(this.renderMessage)}
-        {showWarnings
-          ? warnings.map(this.renderMessage)
-          : warnings.length > 0 &&
-              <Button color="orange" content="Show warnings" onClick={this.handleShowWarningsClick} />}
+        {showWarnings ? (
+          warnings.map(this.renderMessage)
+        ) : (
+          warnings.length > 0 && (
+            <Button color="orange" content="Show warnings" onClick={this.handleShowWarningsClick} />
+          )
+        )}
       </Container>
     );
   }
