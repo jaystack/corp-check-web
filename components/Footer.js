d2e92071fbd230336523e2f601121acb8f21cc4b
Péter Hauszknecht <peter.hauszknecht@guidesmiths.com>
2017-11-16 16:27:37 +0100

    update footer

--- components/Footer.js
+++ components/Footer.js
@@ -4,6 +4,7 @@ import { Container } from 'semantic-ui-react';
 export default () => (
   <footer>
     <Container>
+
       <a href="http://jaystack.com/" target="_blank" className="footer_logo jaystack">
         <img alt="jaystack-logo" src="/static/images/jaystacklogo_white_blue.png" />
         <span>Corporate products from JayStack</span>
