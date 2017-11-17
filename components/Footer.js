a3a6d35d4997b43d162b41a7694251bee3538a30
Péter Hauszknecht <peter.hauszknecht@guidesmiths.com>
2017-11-17 15:18:29 +0100

    add link to footer

--- components/Footer.js
+++ components/Footer.js
@@ -4,10 +4,12 @@ import { Container } from 'semantic-ui-react';
 export default () => (
   <footer>
     <Container>
-
       <a href="http://jaystack.com/" target="_blank" className="footer_logo jaystack">
         <img alt="jaystack-logo" src="/static/images/jaystacklogo_white_blue.png" />
-        <span>Corporate products from JayStack</span>
+        <span>© Copyright - CorpJS.com 2017</span>
+      </a>
+      <a href="http://jaystack.com/" target="_blank" className="link-1">
+        All Developer Tools, Products and Solutions by JayStack
       </a>
       <a href="http://corpjs.com/" target="_blank" className="footer_logo corpjs">
         <img alt="corpjs-logo" src="/static/images/footer_logo.png" />
